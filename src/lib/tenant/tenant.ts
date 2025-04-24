ff3733ebb8414064919034bd861d4e8538333bfb
Joshua Carnide <joshuacarnide@gmail.com>
2025-04-24 01:41:34 -0700

    More config updates

--- src/lib/tenant/tenant.ts
+++ src/lib/tenant/tenant.ts
@@ -1,40 +1,30 @@
-import TenantContractFactory from "@/lib/tenant/tenantContractFactory";
 import { TenantUI } from "@/lib/tenant/tenantUI";
 import {
   type TenantContracts,
   type TenantNamespace,
   type TenantToken,
 } from "../types";
+import { demoTenantConfig } from "./configs/contracts/demo";
 import { nearTenantUIConfig } from "./configs/ui/near";
 
 export const BRAND_NAME_MAPPINGS: Record<string, string> = {
-  ens: "ENS",
-  etherfi: "EtherFi",
-  pguild: "PGuild",
-  boost: "Boost",
-  demo: "Canopy",
+  near: "NEAR",
 };
 
 export default class Tenant {
   private static instance: Tenant;
 
-  private readonly _contracts: TenantContracts;
   private readonly _isProd: boolean;
-  private readonly _namespace: TenantNamespace;
 
   private constructor() {
-    this._namespace = process.env
-      .NEXT_PUBLIC_AGORA_INSTANCE_NAME as TenantNamespace;
     this._isProd = process.env.NEXT_PUBLIC_AGORA_ENV === "prod";
-    this._contracts = TenantContractFactory.create(
-      this._namespace,
-      this._isProd,
-      process.env.NEXT_PUBLIC_ALCHEMY_ID as string
-    );
   }
 
   public get contracts(): TenantContracts {
-    return this._contracts;
+    return demoTenantConfig({
+      isProd: this._isProd,
+      alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_ID as string,
+    });
   }
 
   public get isProd(): boolean {
