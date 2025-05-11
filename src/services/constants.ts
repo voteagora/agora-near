476de6d945383a2719fcfaa16da7f76614c8c61d
Joshua Carnide <joshuacarnide@gmail.com>
2025-05-10 21:19:44 -0700

    Update route

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -1,5 +1,5 @@
 export const baseApiUrl = process.env.NEXT_PUBLIC_NEAR_API_ENDPOINT;
 
 export const Endpoint = Object.freeze({
-  GetProposalVotingHistory: `${baseApiUrl}/proposal-voting-history`,
+  GetProposalVotingHistory: `${baseApiUrl}/proposal/votes`,
 });
