f2d58d9dc80e08d367bb17cc5f72cbfa7a3d462e
Joshua Carnide <joshuacarnide@gmail.com>
2025-05-19 18:47:25 -0700

    Fix types

--- src/components/DelegateStatement/CurrentDelegateStatement.tsx
+++ src/components/DelegateStatement/CurrentDelegateStatement.tsx
@@ -68,7 +68,7 @@ export default function CurrentDelegateStatement() {
         warpcast: delegateProfile?.warpcast || "",
         topIssues:
           (delegateProfile?.topIssues ?? []).length > 0
-            ? delegateProfile?.topIssues
+            ? (delegateProfile?.topIssues ?? defaultIssues)
             : defaultIssues,
       };
     },
