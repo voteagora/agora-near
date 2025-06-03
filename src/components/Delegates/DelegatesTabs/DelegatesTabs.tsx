2dd78c79cb9f95746f17d1bdae590e6fb78e1011
Paul Bao <paul@boosted.technology>
2025-06-03 16:13:06 -0400

    save

--- src/components/Delegates/DelegatesTabs/DelegatesTabs.tsx
+++ src/components/Delegates/DelegatesTabs/DelegatesTabs.tsx
@@ -6,6 +6,7 @@ import { AlignJustify, LayoutGrid } from "lucide-react";
 import { useQueryState } from "nuqs";
 import { useTransition, type ReactNode } from "react";
 import DelegatesSortFilter from "@/components/Delegates/DelegatesFilter/DelegatesSortFilter";
+import DelegatesSearch from "@/components/Delegates/DelegatesSearch/DelegatesSearch";
 
 export default function DelegateTabs({ children }: { children: ReactNode }) {
   const [isPending, startTransition] = useTransition();
@@ -35,7 +36,8 @@ export default function DelegateTabs({ children }: { children: ReactNode }) {
             Delegates
           </TabsTrigger>
         </TabsList>
-        <div className="flex flex-col sm:flex-row justify-between gap-3 w-full sm:w-fit overflow-x-auto">
+        <div className="flex flex-col sm:flex-row justify-between gap-3 w-full sm:w-fit">
+          <DelegatesSearch />
           <DelegatesSortFilter />
           <div className="flex items-center gap-2 bg-wash rounded-full px-4 py-2 shrink-0">
             <button
