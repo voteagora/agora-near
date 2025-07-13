197446fb3ccd2e3f47341741f2698965af2ad950
Joshua Carnide <joshuacarnide@gmail.com>
2025-07-12 20:54:28 -0700

    Default URL to prod

--- src/lib/api/constants.ts
+++ src/lib/api/constants.ts
@@ -1,9 +1,9 @@
 export const baseApiUrl =
   process.env.NEXT_PUBLIC_AGORA_ENV === "dev"
     ? "https://near-api-158107670134.us-west1.run.app/api"
-    : process.env.NEXT_PUBLIC_AGORA_ENV === "prod"
-      ? "https://near-api-237405837378.us-west1.run.app/api"
-      : "http://localhost:8080/api";
+    : process.env.NEXT_PUBLIC_AGORA_ENV === "local"
+      ? "http://localhost:8080/api"
+      : "https://near-api-237405837378.us-west1.run.app/api";
 
 export const Endpoint = Object.freeze({
   Proposals: `${baseApiUrl}/proposal`,
