31f410355ec3768d9b132c37ba9f4afa372b05fb
Paul Bao <paul@boosted.technology>
2025-07-25 10:18:53 -0400

    fix type issue

--- src/lib/contractConstants.ts
+++ src/lib/contractConstants.ts
@@ -1,6 +1,6 @@
 // TODO: Eventually this will be a full config with testnet contracts, mainnet contracts, ABIs, etc...
 export const TESTNET_CONTRACTS = {
-  VENEAR_CONTRACT_ID: process.env.NEXT_PUBLIC_NEAR_CONTRACT_ID,
-  VOTING_CONTRACT_ID: process.env.NEXT_PUBLIC_NEAR_VOTING_CONTRACT_ID,
-  STAKING_POOL_ID: process.env.NEXT_PUBLIC_NEAR_STAKING_POOL_ID,
+  VENEAR_CONTRACT_ID: process.env.NEXT_PUBLIC_NEAR_CONTRACT_ID ?? "",
+  VOTING_CONTRACT_ID: process.env.NEXT_PUBLIC_NEAR_VOTING_CONTRACT_ID ?? "",
+  STAKING_POOL_ID: process.env.NEXT_PUBLIC_NEAR_STAKING_POOL_ID ?? "",
 };
