b37c12af20b4dbefdd7109ec936b21e12b7beed3
Yinan Bao <paul@boosted.technology>
2025-08-07 01:25:26 -0400

    [AXB-208] add og api endpoints (#94)
    * Revert og endpoints
    
    * Ad back og endpoints
    
    * fix
    
    * fix type
    
    * update text
    
    * fix type
    
    * fix type
    
    * Update delegates OG image
    
    * OG image for delegate profile page
    
    * Prettier
    
    * Add OG image for proposal details page
    
    ---------
    
    Co-authored-by: Joshua Carnide <joshuacarnide@gmail.com>

--- src/app/delegates/[accountId]/page.tsx
+++ src/app/delegates/[accountId]/page.tsx
@@ -1,6 +1,9 @@
 import { Metadata } from "next";
 
 import { DelegateProfileContent } from "@/components/Delegates/DelegateProfile/DelegateProfileContent";
+import { getDelegate } from "@/lib/api/delegates/requests";
+import { formatNumber } from "@/lib/utils";
+import { NEAR_TOKEN } from "@/lib/constants";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
@@ -12,12 +15,41 @@ export async function generateMetadata({
 }): Promise<Metadata> {
   const address = params.accountId;
 
+  const delegateProfile = await getDelegate(
+    address,
+    process.env.NEXT_PUBLIC_AGORA_ENV === "prod" ? "mainnet" : "testnet"
+  );
+
+  const statement = delegateProfile?.statement;
+  const votingPower = delegateProfile?.votingPower;
+
+  const imgParams = [
+    votingPower &&
+      `votes=${encodeURIComponent(
+        `${formatNumber(votingPower || "0", NEAR_TOKEN.decimals)} veNEAR`
+      )}`,
+    statement && `statement=${encodeURIComponent(statement)}`,
+  ].filter(Boolean);
+
+  const preview = `/api/images/og/delegate?${imgParams.join(
+    "&"
+  )}&address=${address}`;
+
   const title = `${address} on Agora`;
   const description = `See what ${address} believes and how they vote on NEAR governance.`;
 
   return {
     title: title,
     description: description,
+    openGraph: {
+      images: [
+        {
+          url: preview,
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
     twitter: {
       card: "summary_large_image",
       title,
