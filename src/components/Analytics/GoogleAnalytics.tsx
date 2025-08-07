df364fa1623b4ea1d5e06f818bf09b17dc85599b
Jeff <jeff@voteagora.com>
2025-08-07 13:09:49 -0400

    prettier

--- src/components/Analytics/GoogleAnalytics.tsx
+++ src/components/Analytics/GoogleAnalytics.tsx
@@ -1,8 +1,8 @@
-import { GoogleAnalytics as GA } from '@next/third-parties/google';
+import { GoogleAnalytics as GA } from "@next/third-parties/google";
 
 export function GoogleAnalytics() {
   const measurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
-  
+
   if (!measurementId) {
     return null;
   }
