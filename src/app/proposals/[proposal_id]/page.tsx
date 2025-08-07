f37536481bc800e24478484c5afe0727e6741d40
Joshua Carnide <joshuacarnide@gmail.com>
2025-08-06 22:15:24 -0700

    Add OG image for proposal details page

--- src/app/proposals/[proposal_id]/page.tsx
+++ src/app/proposals/[proposal_id]/page.tsx
@@ -1,8 +1,49 @@
 import ProposalHome from "@/components/Proposals/Proposal/ProposalHome";
-import React from "react";
+import { fetchProposal } from "@/lib/api/proposal/requests";
+import { cleanString, truncateString } from "@/lib/text";
+import { Metadata } from "next";
 
 export const dynamic = "force-dynamic";
 export const maxDuration = 60;
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { proposal_id: string };
+}): Promise<Metadata> {
+  const proposalId = params.proposal_id;
+
+  const proposal = await fetchProposal(proposalId);
+
+  const title = truncateString(cleanString(proposal?.title || ""), 40);
+  const description = truncateString(
+    cleanString(proposal?.description || ""),
+    80
+  );
+
+  const preview = `/api/images/og/generic?title=${encodeURIComponent(
+    title
+  )}&description=${encodeURIComponent(description)}`;
+
+  return {
+    title: title,
+    description: description,
+    openGraph: {
+      images: [
+        {
+          url: preview,
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+    },
+  };
+}
 
 export default async function Page({
   params: { proposal_id },
