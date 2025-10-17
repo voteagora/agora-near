964e39743c261c9de1df51c4078fb96609975d2b
Atomauro <mauro.henaog@gmail.com>
2025-10-17 15:20:18 -0500

    feat: add GlobalError component and enhance ResourceNotFound with customizable props. 404 page.

--- src/app/assets/page.tsx
+++ src/app/assets/page.tsx
@@ -13,7 +13,7 @@ export async function generateMetadata() {
   )}&description=${encodeURIComponent(imageDescription)}`;
 
   return {
-    title: title,
+    title: "RNEAR ORIGINAL",
     description: description,
     openGraph: {
       images: [
