654ef90c65c33c6c46ea707db0e3b6dac13a44ad
Joshua Carnide <joshuacarnide@gmail.com>
2025-10-18 19:09:40 -0700

    Refactor wallet and lockup holding components

--- src/components/Assets/HoldingsContent.tsx
+++ src/components/Assets/HoldingsContent.tsx
@@ -1,129 +1,22 @@
-import { filterDust } from "@/lib/tokenUtils";
-import { LockupLiquidBalance, TokenWithBalance } from "@/lib/types";
-import { memo, useMemo } from "react";
-import { LockableAssetRow } from "./LockableAssetRow";
-import { VeNearAssetRow } from "./VeNearAssetRow";
-import { LockupLiquidNearRow } from "./VeNearLiquidAssetRow";
-import { VeNearStakedAssetRow } from "./VeNearStakedAssetRow";
-import Big from "big.js";
+import { memo } from "react";
+import { LockupHoldings } from "./LockupHoldings";
+import { WalletHoldings } from "./WalletHoldings";
 
 interface HoldingsContentProps {
-  liquidLockupBalance: LockupLiquidBalance;
-  walletTokens: TokenWithBalance[];
-  unlockTimestamp: string | null | undefined;
-  lockupAccountId: string | null | undefined;
-  balanceWithRewards: string;
-  hasPendingBalance: boolean;
-  pendingBalance: string | undefined;
-  isEligibleToUnlock: boolean;
-  stakingPoolId: string | null | undefined;
-  stakedBalance: string | null | undefined;
   openLockDialog: (preSelectedTokenId?: string | null) => void;
   openStakingDialog: () => void;
 }
 
 export const HoldingsContent = memo(
-  ({
-    liquidLockupBalance,
-    walletTokens,
-    unlockTimestamp,
-    lockupAccountId,
-    balanceWithRewards,
-    hasPendingBalance,
-    pendingBalance,
-    isEligibleToUnlock,
-    stakingPoolId,
-    stakedBalance,
-    openLockDialog,
-    openStakingDialog,
-  }: HoldingsContentProps) => {
-    // TODO: Fix root cause of dust amounts remaining after "Max" lock
-    const filteredWalletTokens = useMemo(
-      () =>
-        walletTokens.map((token) => ({
-          ...token,
-          balance: filterDust({ amount: token.balance }),
-        })),
-      [walletTokens]
-    );
-
-    const filteredLiquidLockupBalance = useMemo(() => {
-      return {
-        withdrawableNearBalance: filterDust({
-          amount: liquidLockupBalance.withdrawableNearBalance ?? "0",
-        }),
-        stakableNearBalance: filterDust({
-          amount: liquidLockupBalance.stakableNearBalance ?? "0",
-        }),
-        lockableNearBalance: filterDust({
-          amount: liquidLockupBalance.lockableNearBalance ?? "0",
-        }),
-      };
-    }, [
-      liquidLockupBalance.lockableNearBalance,
-      liquidLockupBalance.stakableNearBalance,
-      liquidLockupBalance.withdrawableNearBalance,
-    ]);
-
-    const hasLiquidNearInLockup = useMemo(
-      () =>
-        Big(filteredLiquidLockupBalance.lockableNearBalance ?? "0").gt(0) ||
-        Big(filteredLiquidLockupBalance.stakableNearBalance ?? "0").gt(0) ||
-        Big(filteredLiquidLockupBalance.withdrawableNearBalance ?? "0").gt(0),
-      [
-        filteredLiquidLockupBalance.lockableNearBalance,
-        filteredLiquidLockupBalance.stakableNearBalance,
-        filteredLiquidLockupBalance.withdrawableNearBalance,
-      ]
-    );
-
+  ({ openLockDialog, openStakingDialog }: HoldingsContentProps) => {
     return (
       <table className="w-full">
         <tbody>
-          <tr>
-            <td colSpan={3} className="pb-3">
-              <h3 className="text-lg font-semibold">Lockup Holdings</h3>
-            </td>
-          </tr>
-          <VeNearAssetRow
-            unlockTimestamp={unlockTimestamp ?? undefined}
-            lockupAccountId={lockupAccountId ?? undefined}
-            balanceWithRewards={balanceWithRewards}
-            hasPendingBalance={hasPendingBalance}
-            pendingBalance={pendingBalance}
-            isEligibleToUnlock={isEligibleToUnlock}
+          <LockupHoldings
+            openLockDialog={openLockDialog}
+            openStakingDialog={openStakingDialog}
           />
-          {hasLiquidNearInLockup && (
-            <LockupLiquidNearRow
-              liquidLockupBalance={filteredLiquidLockupBalance}
-              onStakeClick={openStakingDialog}
-              onLockClick={openLockDialog}
-              lockupAccountId={lockupAccountId}
-            />
-          )}
-          {stakingPoolId && (
-            <VeNearStakedAssetRow
-              stakingPoolId={stakingPoolId}
-              stakedBalance={stakedBalance ?? "0"}
-            />
-          )}
-          {filteredWalletTokens.length > 0 && (
-            <>
-              <tr>
-                <td colSpan={3} className="pt-8 pb-3">
-                  <h3 className="text-lg font-semibold">Wallet Holdings</h3>
-                </td>
-              </tr>
-              {filteredWalletTokens.map((token) => (
-                <LockableAssetRow
-                  key={token.accountId}
-                  token={token}
-                  stakingPoolId={stakingPoolId ?? undefined}
-                  onLockClick={openLockDialog}
-                />
-              ))}
-            </>
-          )}
+          <WalletHoldings openLockDialog={openLockDialog} />
         </tbody>
       </table>
     );
--- src/components/Assets/HoldingsSection.tsx
+++ src/components/Assets/HoldingsSection.tsx
@@ -1,17 +1,8 @@
 import { useNear } from "@/contexts/NearContext";
-import { useLiquidWalletTokens } from "@/hooks/useAvailableTokens";
-import { useCurrentStakingPoolId } from "@/hooks/useCurrentStakingPoolId";
-import { useLockupAccount } from "@/hooks/useLockupAccount";
-import { useLockupPendingBalance } from "@/hooks/useLockupPendingBalance";
-import { useStakedBalance } from "@/hooks/useStakedBalance";
-import { useVenearAccountInfo } from "@/hooks/useVenearAccountInfo";
-import Big from "big.js";
-import { memo, useCallback, useMemo, useState } from "react";
+import { memo, useCallback, useState } from "react";
 import { useOpenDialog } from "../Dialogs/DialogProvider/DialogProvider";
-import { Skeleton } from "../ui/skeleton";
 import { HoldingsContent } from "./HoldingsContent";
 import { HosActivityTable } from "./HosActivityTable";
-import { useLiquidLockupBalance } from "@/hooks/useLiquidLockupBalance";
 
 export const HoldingsSection = memo(() => {
   const [activeTab, setActiveTab] = useState<"Holdings" | "Activity">(
@@ -23,49 +14,6 @@ export const HoldingsSection = memo(() => {
   const handleTabClick = useCallback((tab: "Holdings" | "Activity") => {
     setActiveTab(tab);
   }, []);
-
-  const {
-    isLoading: isLoadingLiquidWalletTokens,
-    availableTokens: liquidWalletTokens,
-  } = useLiquidWalletTokens();
-
-  const { lockupAccountId, isLoading: isLoadingLockupAccountId } =
-    useLockupAccount();
-
-  const { isLoading: isLoadingLiquidLockupBalance, liquidLockupBalance } =
-    useLiquidLockupBalance({ lockupAccountId });
-
-  const { data: accountInfo, isLoading: isLoadingAccountInfo } =
-    useVenearAccountInfo(signedAccountId);
-
-  const {
-    unlockTimestamp,
-    pendingBalance,
-    isEligibleToUnlock,
-    hasPendingBalance,
-    isLoading: isLoadingLockupPendingBalance,
-  } = useLockupPendingBalance({
-    lockupAccountId: lockupAccountId,
-  });
-
-  const balanceWithRewards = useMemo(
-    () =>
-      Big(accountInfo?.totalBalance.near ?? "0")
-        .add(accountInfo?.totalBalance.extraBalance ?? "0")
-        .toFixed(),
-    [accountInfo]
-  );
-
-  const { stakingPoolId, isLoadingStakingPoolId } = useCurrentStakingPoolId({
-    lockupAccountId,
-  });
-
-  const { stakedBalance, isLoading: isLoadingStakedBalance } = useStakedBalance(
-    {
-      stakingPoolId,
-      accountId: lockupAccountId,
-    }
-  );
 
   const openDialog = useOpenDialog();
 
@@ -91,31 +39,6 @@ export const HoldingsSection = memo(() => {
       },
     });
   }, [openDialog]);
-
-  const walletTokens = useMemo(
-    () => liquidWalletTokens.filter((token) => token.type !== "lockup"),
-    [liquidWalletTokens]
-  );
-
-  const isLoading =
-    isLoadingLiquidWalletTokens ||
-    isLoadingLockupAccountId ||
-    isLoadingStakingPoolId ||
-    isLoadingAccountInfo ||
-    isLoadingLockupPendingBalance ||
-    isLoadingStakedBalance ||
-    isLoadingLiquidLockupBalance;
-
-  if (isLoading) {
-    return (
-      <div className="flex flex-col gap-4">
-        <Skeleton className="h-14 w-full" />
-        <Skeleton className="h-14 w-full" />
-        <Skeleton className="h-14 w-full" />
-        <Skeleton className="h-14 w-full" />
-      </div>
-    );
-  }
 
   return (
     <div className="flex-1 sm:px-6 pb-6">
@@ -146,16 +69,6 @@ export const HoldingsSection = memo(() => {
         <div className="sm:px-6 py-6 px-2">
           {activeTab === "Holdings" ? (
             <HoldingsContent
-              liquidLockupBalance={liquidLockupBalance}
-              walletTokens={walletTokens}
-              unlockTimestamp={unlockTimestamp}
-              lockupAccountId={lockupAccountId}
-              balanceWithRewards={balanceWithRewards}
-              hasPendingBalance={hasPendingBalance}
-              pendingBalance={pendingBalance}
-              isEligibleToUnlock={isEligibleToUnlock}
-              stakingPoolId={stakingPoolId}
-              stakedBalance={stakedBalance}
               openLockDialog={openLockDialog}
               openStakingDialog={openStakingDialog}
             />
