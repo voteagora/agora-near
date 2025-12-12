67f4e388fcb5e13b15565458f51a0a6e006eee17
Atomauro <mauro.henaog@gmail.com>
2025-12-12 14:50:36 -0500

    Adding MockWalletSelector

---
+++ src/lib/tests/MockWalletSelector.ts
@@ -0,0 +1,41 @@
+
+import { WalletSelector, Wallet } from "@near-wallet-selector/core";
+
+export class MockWalletSelector {
+  isSignedIn() {
+    return true;
+  }
+
+  store = {
+    getState: () => ({
+      accounts: [{ accountId: "mock-user.testnet", active: true }],
+    }),
+    observable: {
+      subscribe: () => ({ unsubscribe: () => {} }),
+    },
+  };
+
+  wallet(): Promise<Wallet> {
+    return Promise.resolve({
+      signIn: async () => [],
+      signOut: async () => {},
+      getAccounts: async () => [{ accountId: "mock-user.testnet", active: true }],
+      signAndSendTransaction: async (params: any) => {
+        console.log("Mock calling signAndSendTransaction", params);
+        return {
+          transaction_outcome: { id: "mock-tx-hash" },
+          receipts_outcome: [],
+        };
+      },
+      signAndSendTransactions: async (params: any) => {
+         console.log("Mock calling signAndSendTransactions", params);
+         return [
+           {
+             transaction_outcome: { id: "mock-tx-hash" },
+             receipts_outcome: [],
+           }
+         ];
+      }
+    } as unknown as Wallet);
+  }
+}
